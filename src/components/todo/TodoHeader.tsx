852e53d5949e96d1ae243bad46d1e4baceba7dcf
hyeji <hyeji6742@naver.com>
2025-07-24 01:44:55 +0900

    feat: todoHeader 구현

---
+++ src/components/todo/TodoHeader.tsx
@@ -0,0 +1,21 @@
+import { Button } from "../ui/button";
+
+type Props ={
+    todoCount: number;
+
+    onClickAdd: () => void;
+}
+
+export default function TodoHeader({todoCount, onClickAdd}: Props){
+    return(
+        <div>
+            <div className="flex justify-between items-center bg-amber-300">
+                <h1 className="text-3xl font-bold text-gray-800 mb-2">할일 목록</h1>
+                <Button size="sm" onClick={onClickAdd}>
+                    추가
+                </Button>
+            </div>
+            <p className="text-gray-600">총 {todoCount}개의 할일이 남았습니다.</p>
+        </div>
+    );
+}
--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,5 +1,9 @@
+import TodoHeader from "@/components/todo/TodoHeader"
+
 export default function TodoPage(){
     return(
-        <div>TodoPage!</div>
+        <TodoHeader todoCount={0}>
+            <div>TodoPage!</div>
+        </TodoHeader>
     )
 }

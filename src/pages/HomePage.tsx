cdcfe783fa2fef1c694e41fb9acf3017bcd7823d
hyeji <hyeji6742@naver.com>
2025-07-24 01:30:37 +0900

    feat: HomePage.tsx 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage(){
+    const navigate = useNavigate();
+
+    const handleStart = () => {
+        // replace: true → 현재 페이지를 새로운 경로로 대체, 뒤로가기 시 이 페이지로 못 돌아옴
+        navigate("/todo", {replace:false});
+    }
     return(
-        <div>HomePage!</div>
+        <div className="flex flex-col justify-center items-center h-full bg-pink-100">
+            <Button onClick={handleStart}>시작하기</Button>
+        </div>
     )
 }
